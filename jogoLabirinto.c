f99f6e4acf1fbdc80e69d3d6f6a6482658bb683b
iaguinho1727 <iagossassen2@gmail.com>
2023-06-21 00:51:17 -0300

    Other phases

--- jogoLabirinto.c
+++ jogoLabirinto.c
@@ -8,10 +8,11 @@
 // -5 -> chave
 // -6 -> porta
 
-#define lin 5
-#define col 10
+#define lin 2
+#define col 5
+#define prof 10
 
-void escreverGame(int game[][col]) {
+void escreverGame(int game[][prof]) {
     system("cls");
 
     printf("\n");
@@ -19,20 +20,21 @@ void escreverGame(int game[][col]) {
     printf("# -> Walls; \033[34m @ \033[0m -> Player; \033[31m * \033[0m -> Coin; \033[33m ! \033[0m -> Key; \033[29m | \033[0m -> Door\n\n");
 
     int x,y;
-    for( x = 0 ; x < lin ; x++ ) {
-        for( y = 0 ; y < col ; y++ ) {
+    for( x = 0 ; x < col ; x++ ) {
+        for( y = 0 ; y < prof ; y++ ) {
             if(game[x][y] == -1) printf("\033[00m###\033[0m");
             else if(game[x][y] == -2) printf("   ");
             else if(game[x][y] == -3) printf("\033[34m @ \033[0m");
             else if(game[x][y] == -4) printf("\033[31m * \033[0m");
             else if(game[x][y] == -5) printf("\033[33m ! \033[0m");
             else if(game[x][y] == -6) printf("\033[29m | \033[0m");
+            else printf("%i ", game[x][y]);
         }
         printf("\n");
     }
 }
 
-int movingDone(int game[][col], char toWhere, int playerLoc[2], int over, int thereIs, int *p) {
+int movingDone(int game[][prof], char toWhere, int playerLoc[2], int over, int thereIs, int *p) {
     int playerX = playerLoc[0];
     int playerY = playerLoc[1];
 
@@ -81,10 +83,10 @@ int movingDone(int game[][col], char toWhere, int playerLoc[2], int over, int th
     return over;
 }
 
-void acharPlayer(int game[][col], int locPlayer[2]) {
+void acharPlayer(int game[][prof], int locPlayer[2]) {
     int x,y;
-    for( x = 0 ; x < lin ; x++ ) {
-        for( y = 0 ; y < col ; y++ ) {
+    for( x = 0 ; x < col ; x++ ) {
+        for( y = 0 ; y < prof ; y++ ) {
             if(game[x][y] == -3) {
                 locPlayer[0] = x;
                 locPlayer[1] = y;
@@ -93,36 +95,87 @@ void acharPlayer(int game[][col], int locPlayer[2]) {
     }
 }
 
-int main() {
-    int game[lin][col] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-                          -1, -2, -2, -1, -2, -2, -2, -1, -2, -1,
-                          -1, -5, -1, -1, -3, -1, -2, -6, -4, -1,
-                          -1, -2, -2, -2, -2, -1, -2, -1, -2, -1,
-                          -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+void preencherMapa(int game[][col][prof], int round[][prof], int profu) {
+    int x,y;
+    for( x = 0 ; x < col ; x++ ) {
+        for( y = 0 ; y < prof ; y++ ) {
+            round[x][y] = game[profu][x][y];
+        }
+    } 
+}
 
-                          //{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-                          //-1, -2, -2, -2, -2, -2, -2, -2, -2, -1,
-                          //-1, -2, -2, -2, -2, -2, -2, -2, -2, -1,
-                          //-1, -2, -2, -2, -2, -2, -2, -2, -2, -1,
-                          //-1, -1, -1, -1, -1, -1, -1, -1, -1, -1}
-    escreverGame(game);
+int main() {
+    int game[lin][col][prof] = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+                                -1,-2,-3,-2,-2,-5,-1,-4,-2,-1,
+                                -1,-2,-1,-1,-1,-1,-1,-1,-2,-1,
+                                -1,-2,-2,-6,-2,-2,-2,-2,-2,-1,
+                                -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,//
+                                -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+                                -1,-2,-3,-2,-2,-5,-1,-4,-2,-1,
+                                -1,-2,-1,-1,-1,-1,-1,-1,-2,-1,
+                                -1,-2,-2,-6,-4,-2,-2,-2,-2,-1,
+                                -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,};
+
+                            //    {-1,-1,-1,-1,-1,
+                            //     -1,-2,-3,-2,-1,
+                            //     -1,-1,-2,-1,-1,
+                            //     -1,-2,-2,-2,-1,
+                            //     -1,-2,-1,-5,-1,
+                            //     -1,-6,-1,-1,-1,
+                            //     -1,-2,-1,-2,-1,
+                            //     -1,-2,-1,-1,-1
+                            //     -1,-2,-2,-4,-1,
+                            //     -1,-1,-1,-1,-1,//
+                            //     -1,-1,-1,-1,-1,
+                            //     -1,-1,-3,-4,-1,
+                            //     -1,-1,-2,-1,-1,
+                            //     -1,-2,-2,-2,-1,
+                            //     -1,-2,-1,-5,-1,
+                            //     -1,-6,-1,-1,-1,
+                            //     -1,-2,-1,-2,-1,
+                            //     -1,-2,-1,-1,-1
+                            //     -1,-2,-2,-4,-1,
+                            //     -1,-1,-1,-1,-1}
+
+    int round[col][prof];
+    int profu = 0;
+    preencherMapa(game, round, profu);
+
+    escreverGame(round);
+
 
     int over = 0, playerLoc[2], thereIs = 0, *p = &thereIs;
-    acharPlayer(game, playerLoc);
 
-    while(!over) {
-        char move;
-        printf("\nWrite the next move: ");
-        scanf("%c", &move);
+    int x,y,z;
 
-        over = movingDone(game, move, playerLoc, over, thereIs, p);
+    for( x = 0 ; x < lin ; x++ ) {
+        acharPlayer(round, playerLoc);
+        while(!over) {
+            char move;
+            printf("\nWrite the next move: ");
+            scanf("%c", &move);
+            over = movingDone(round, move, playerLoc, over, thereIs, p);
 
-        escreverGame(game);
+            escreverGame(round);
+        }
+        printf("\nCongrutalion, you find the coin!\n");
+        over--;
+        profu++;
+        // printf("%i\n", profu);
+        // getch();
+        if(profu < lin) preencherMapa(game, round, profu);
     }
 
+    // for(x = 0; x < col; x++) {
+    //     for(y = 0; y < prof; y++) {
+    //         printf("%i", round[x][y]);
+    //     }
+    //     printf("\n");
+    // }
+    getch();
     system("cls");
-    escreverGame(game);
-    printf("\nCongratulation, you find the coin!\n");
+    escreverGame(round);
+    printf("\nThank u for play my game! :)\n");
 
     return 0;
 }
